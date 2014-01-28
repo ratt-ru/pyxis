ed4903c9e4f20a9b82ed3632a8de9ea173a6622b
Gijs Molenaar <gijs@pythonic.nl>
2014-01-28 14:42:37 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+setup(name='pyxis',
+      version='1.3.0',
+      description='',
+      author='Oleg Smirnov',
+      author_email='Oleg Smirnov <osmirnov@gmail.com>',
+      url='https://github.com/ska-sa/pyxis',
+      packages=['Pyxis', 'Pyxides'],
+      requires=['pyfits', 'timba', 'matplotlib', 'pyrap', 'numpy'],
+      scripts=['Pyxis/bin/' + i for i in os.listdir('Pyxis/bin')],
+     )
